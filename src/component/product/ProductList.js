4ad2583e03767d18db392f974103672d51c2c451
nguyencam123 <camnmph26581@fpt.edu.vn>
2023-10-04 21:27:49 +0700

    update component home

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -1,8 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from '../../features/product/productThunk';
 import React, { useEffect, useState } from 'react';
-import { Spin, Card, Row, Col, Typography } from 'antd';
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { Spin, Card, Row, Col, Typography, Input, Select, Button, Carousel } from 'antd';
+import { LeftOutlined, RightOutlined, TeamOutlined } from "@ant-design/icons";
 import imgsection from "../../assets/svg/Ellipse 1.svg"
 import imgslide2 from "../../assets/svg/Ellipse 2.svg"
 import imgslide3 from "../../assets/svg/Ellipse 3.svg"
@@ -11,9 +11,13 @@ import beach from "../../assets/svg/beach_access_FILL0_wght400_GRAD0_opsz24.svg"
 import kayaking from "../../assets/svg/kayaking_FILL0_wght400_GRAD0_opsz24.svg"
 import slideform from "../../assets/svg/Rectangle 7.svg"
 import "./ProductList.css"
-import { Carousel } from 'antd';
-
+import dayjs from 'dayjs';
+import { DatePicker, Space } from 'antd';
+const { Search } = Input;
 const Title = Typography
+const { RangePicker } = DatePicker;
+
+const dateFormat = 'YYYY/MM/DD';
 const contentStyle = {
     height: '45%',
     color: '#fff',
@@ -23,7 +27,7 @@ const contentStyle = {
     display: 'flex'
 };
 const sectionStyle = {
-    marginTop: '300px',
+    marginTop: '250px',
     height: "323px",
     width: "100%",
 
@@ -67,7 +71,11 @@ function ProductList() {
         { img: imgslide4, title: 'Hội An' },
         { img: imgslide4, title: 'Hội An' },
         { img: imgslide4, title: 'Hội An' },
-        { img: imgslide4, title: 'Đà Lạt' }
+        { img: imgslide4, title: 'Đà Lạt' },
+        { img: imgsection, title: 'Hồ Chí Minh' },
+        { img: imgslide2, title: 'Hà Nội' },
+        { img: imgslide3, title: 'Hải Phòng' },
+        { img: imgslide4, title: 'Đà Lạt' },
         // Thêm các dữ liệu khác tương tự
     ];
     const itemsPerPage = 7;
@@ -75,6 +83,7 @@ function ProductList() {
     const endIndex = startIndex + itemsPerPage;
 
     const itemsToShow = YourData.slice(startIndex, endIndex); // Thay YourData bằng dữ liệu thực tế của bạn
+    const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
 
     return (
 
@@ -83,7 +92,7 @@ function ProductList() {
             <div>
                 <h3 style={contentStyle}>
                     <div style={{ position: 'relative' }}>
-                        <img src={slideform} style={{ width: '100%' }} />
+                        <img src={slideform} style={{ width: '100%', backgroundSize: 'auto' }} />
                         <div style={{
                             position: 'absolute',
                             left: '50%',
@@ -95,20 +104,52 @@ function ProductList() {
                             borderRadius: '8px',
                             backgroundColor: '#FFE5CA',
                             minHeight: '100px',
-                            minWidth: '150px'
-                        }}></div>
+                            minWidth: '150px',
+                            display: 'flex', /* Thêm thuộc tính display: flex để căn giữa nội dung bên trong */
+                            flexDirection: 'column', /* Dọc xuống */
+                            alignItems: 'center', /* Căn giữa theo chiều ngang */
+                            justifyContent: 'space-between'
+                        }}>          <Search placeholder="input search text" size="large" onSearch={onSearch} enterButton style={{ width: '80%', marginTop: '20px', fontSize: '20px' }} />
+                            <div style={{ position: 'absolute', top: 30 }}>
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <RangePicker size='large'
+                                                defaultValue={[dayjs('2023/09/01', dateFormat), dayjs('2023/10/01', dateFormat)]}
+                                                format={dateFormat}
+                                            />&emsp;
+                                        </td>
+                                        <td>
+                                            <Select
+                                                defaultValue="lucy"
+                                                style={{ width: '317px', minWidth: '150px' }} // Thêm maxWidth ở đây
+                                                size='large'
+                                                options={[
+                                                    { value: 'jack', label: '2 người lớn một phòng' },
+                                                    { value: 'lucy', label: 'Lucy' },
+                                                    { value: 'Yiminghe', label: 'yiminghe' },
+                                                    { value: 'disabled', label: 'Disabled', disabled: true },
+                                                ]}
+                                                suffixIcon={<TeamOutlined style={{ position: 'absolute', right: '10px' }} />}
+                                            />
+                                        </td>
+                                    </tr>
+                                </table>
+
+                            </div>
+                            <div>
+                                <Button style={{ width: 530, height: 64, borderRadius: 16, backgroundColor: '#FF7D63', fontSize: '20', color: '#FFFFFF', top: 80 }}>Tìm kiếm</Button>
+                            </div>
+                        </div>
                     </div>
                 </h3>
             </div>
-
-
-
             <section style={sectionStyle}>
                 <div className='texthead' style={{ textAlign: 'center' }}>
                     Các Điểm Đến Thu Hút Nhất Việt Nam
                 </div>
                 <div className='allItem' style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                    {currentPage > 0 && <div onClick={handlePrevPage}><LeftOutlined /></div>}
+                    {currentPage > 0 && <div onClick={handlePrevPage}><LeftOutlined style={{ fontSize: '22px' }} /></div>}
                     {itemsToShow.map((item, index) => (
                         <div className='picturetrval' style={{ textAlign: 'center', width: '10%' }} key={index}>
                             <div className='image-container'>
@@ -119,7 +160,7 @@ function ProductList() {
                             </div>
                         </div>
                     ))}
-                    {(currentPage + 1) * itemsPerPage < YourData.length && <div onClick={handleNextPage}><RightOutlined /></div>}
+                    {(currentPage + 1) * itemsPerPage < YourData.length && <div onClick={handleNextPage}><RightOutlined style={{ fontSize: '22px', position: 'absolute' }} /></div>}
                 </div>
             </section>
             <br /><br />
