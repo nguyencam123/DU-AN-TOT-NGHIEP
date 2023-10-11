da5f234019582c53322d51a03bedfcf22867db5e
nguyencam123 <camnmph26581@fpt.edu.vn>
2023-10-11 15:55:33 +0700

    update hotel component

--- src/component/user/hotelcomponent/hotel.js
+++ src/component/user/hotelcomponent/hotel.js
@@ -1,5 +1,6 @@
-import { Typography, DatePicker, Select, Input, Collapse, Button } from "antd"
+import { Typography, DatePicker, Select, Input, Collapse, Button, Layout, Slider, Checkbox, Rate } from "antd"
 import hotelimg from "../../../assets/img/saleHotel.png"
+import logoTravel from "../../../assets/svg/Rectangle 405.svg"
 import {
     SearchOutlined,
     FireOutlined,
@@ -17,6 +18,7 @@ import { useState } from "react";
 import 'dayjs/locale/vi';
 dayjs.locale('vi');
 const { Title } = Typography
+const { Header, Footer, Sider, Content } = Layout;
 
 
 const text = <section>
@@ -25,11 +27,13 @@ const text = <section>
     </div><br />
     <div style={{ justifyContent: 'space-between', display: 'flex', fontSize: 18 }}>
         <div style={{ display: 'flex' }}><WalletOutlined style={{ marginTop: 4 }} />&ensp;Trẻ em</div><div><Input style={{ width: 40, height: 40 }} defaultValue={1} /></div>
-    </div><br />
-    <div style={{ justifyContent: 'space-between', display: 'flex', fontSize: 18 }}>
-        <div style={{ display: 'flex' }}><UsergroupAddOutlined style={{ marginTop: 4 }} />&ensp;Phòng</div><div><Input style={{ width: 40, height: 40 }} defaultValue={1} /></div>
     </div>
 </section>;
+const utilities = <div style={{ width: '100%', height: '30%', backgroundColor: 'white', borderRadius: 10, padding: '5px 5px 5px' }}>
+    <div>
+
+    </div>
+</div>
 const items = [
     {
         key: '1',
@@ -37,6 +41,13 @@ const items = [
         children: <p>{text}</p>,
     }
 ];
+const itemsutilities = [
+    {
+        key: '2',
+        label: 'Tiện nghi',
+        children: <p>{utilities}</p>,
+    }
+]
 
 const Hotel = () => {
     const onChange = (key) => {
@@ -85,6 +96,29 @@ const Hotel = () => {
         }
         return '';
     };
+    const [rangeValue, setRangeValue] = useState([0, 25000000]);
+    const initialValue = [0, 25000000];
+    const handleSliderChange = (value) => {
+        // Kiểm tra nếu giá trị trái lớn hơn giá trị phải
+        if (value[0] > value[1]) {
+            return;
+        }
+        setRangeValue(value);
+    };
+    const handleInputChange = (index, value) => {
+        const newValue = [...rangeValue];
+        newValue[index] = value;
+
+        // Kiểm tra nếu khoảng cách giữa giá trị trái và giá trị phải lớn hơn 20000
+        if (newValue[1] - newValue[0] < 20000) {
+            return;
+        }
+
+        setRangeValue(newValue);
+    };
+    const handleresetinput = () => {
+        setRangeValue(initialValue);
+    }
     return (
         <>
             <section style={{
@@ -154,6 +188,50 @@ const Hotel = () => {
                     </div>
                 </div>
 
+            </section>
+            <section style={{ padding: '20px 200px 400px 200px' }}>
+                <Layout hasSider >
+                    <Sider style={{ marginRight: 40, backgroundColor: '#f5f5f5' }}>
+                        <img src={logoTravel} /><br /><br />
+                        <div style={{ display: 'flex', width: '100%', height: '30%', backgroundColor: 'white', borderRadius: 10, padding: '5px 5px 5px' }}>
+                            <div>
+                                <div style={{ display: 'flex' }}>
+                                    <Title level={2} style={{ fontSize: 16 }}>Phạm vi giá</Title>
+                                    <Title level={2} style={{ fontSize: 16, marginLeft: 40, color: '#0194f3', cursor: 'pointer' }} onClick={handleresetinput}>Đặt lại</Title>
+                                </div>
+                                <Title level={2} style={{ fontSize: 12 }}>1 phòng 1 đêm</Title>
+                                <div style={{ display: 'flex' }}>
+                                    <Input style={{ width: 90, height: 20, fontSize: 10 }} value={rangeValue[0]}
+                                        onChange={(e) => handleInputChange(0, e.target.value)} prefix='VND' />&ensp;
+                                    <Input style={{ width: 90, height: 20, fontSize: 10 }} value={rangeValue[1]}
+                                        onChange={(e) => handleInputChange(1, e.target.value)} prefix='VND' />
+                                </div>
+                                <Slider
+                                    range={{
+                                        draggableTrack: true,
+                                    }}
+                                    min={0}
+                                    max={25000000}
+                                    value={rangeValue}
+                                    step={20000}
+                                    onChange={handleSliderChange}
+                                />
+                            </div>
+                        </div><br />
+                        <div style={{ width: '100%', height: '45%', backgroundColor: 'white', borderRadius: 10, padding: '5px 5px 5px' }}>
+                            <div>
+                                <Title level={2} style={{ fontSize: 16 }}>Phạm vi giá</Title>
+                                <Checkbox><Rate allowHalf disabled defaultValue={1} /></Checkbox>
+                                <Checkbox><Rate allowHalf disabled defaultValue={2} /></Checkbox>
+                                <Checkbox><Rate allowHalf disabled defaultValue={3} /></Checkbox>
+                                <Checkbox><Rate allowHalf disabled defaultValue={4} /></Checkbox>
+                                <Checkbox><Rate allowHalf disabled defaultValue={5} /></Checkbox>
+                            </div>
+                        </div><br />
+                        <Collapse items={itemsutilities} size="small" defaultActiveKey={['2']} onChange={onChange} />
+                    </Sider>
+                    <Content style={{}}>Content</Content>
+                </Layout>
             </section>
         </>
     )
