7c6468b3a856c8ba42928ed0bd6aae1788b73cc2
Nguyễn Quốc Cường <106654561+qcuongg@users.noreply.github.com>
2023-10-13 02:49:10 +0700

    Update CancellationPolicyRoom.java

--- BE/src/main/java/com/example/demo/entities/CancellationPolicyRoom.java
+++ BE/src/main/java/com/example/demo/entities/CancellationPolicyRoom.java
@@ -1,6 +1,5 @@
 package com.example.demo.entities;
 
-
 import com.example.demo.entities.base.PrimaryEntity;
 import com.example.demo.infrastructure.contant.EntityProperties;
 import jakarta.persistence.Column;
@@ -17,25 +16,12 @@ import java.math.BigDecimal;
 @Setter
 public class CancellationPolicyRoom extends PrimaryEntity {
 
-    private BigDecimal price;
-
-    @Column(length = EntityProperties.LENGTH_NAME)
-@Table(name = "cancellationpolicyroom")
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Data
-public class CancellationPolicyRoom {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Integer id;
-
     @Column(name = "name")
     private String name;
 
     @Column(length = EntityProperties.LENGTH_NOTE, name = "[desc]")
     private String desc;
 
+    @Column(length = EntityProperties.LENGTH_NAME)
+    private BigDecimal price;
 }
