e743f1240257f66f10df24bbc069fb6477bbe7be
qcuongg <cuongnguyen.nd2015@gmail.com>
2023-10-20 10:38:10 +0700

    Update cau hinh project

--- BE/src/main/java/com/example/demo/cors/admin/controller/AdminHomestayController.java
+++ BE/src/main/java/com/example/demo/cors/admin/controller/AdminHomestayController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v3/homestay")
 public class AdminHomestayController {
+
     @Autowired
     private IAdminHomestaySevice iAdminHomestaySevice;
 
