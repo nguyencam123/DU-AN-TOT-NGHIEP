0235ed25711d206e18be7bbd662b8c035708bf20
Hoanbqph28771 <hoanbqph28771@fpt.edu.vn>
2023-11-13 05:31:10 +0700

    fixError

--- BE/src/main/java/com/example/demo/cors/admin/model/request/AdminBookingRequest.java
+++ BE/src/main/java/com/example/demo/cors/admin/model/request/AdminBookingRequest.java
@@ -1,15 +1,25 @@
 package com.example.demo.cors.admin.model.request;
 
 import com.example.demo.cors.common.base.PageableRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminBookingRequest extends PageableRequest {
 
-    private String userName;
+     public String userName;
 
-    private String homestayName;
+     public String homestayName;
+
+     public String sdtUser;
+
+     public String nameBooking;
+
+     public Integer statusBooking;
 
 }
