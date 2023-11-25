9c415a75a79d69e8a2bf3c645ce2f032fa0b5b86
sangvuong <sangvtph26073@fpt.edu.vn>
2023-11-25 17:54:12 +0700

    update

--- BE/src/main/java/com/example/projectstool/DBGenerator.java
+++ BE/src/main/java/com/example/projectstool/DBGenerator.java
@@ -21,6 +21,9 @@ import com.example.demo.infrastructure.contant.Status;
 import com.example.demo.infrastructure.contant.StatusBooking;
 import com.example.demo.infrastructure.contant.StatusCart;
 import com.example.demo.infrastructure.contant.TypePromotion;
+import com.example.demo.infrastructure.contant.role.RoleAdmin;
+import com.example.demo.infrastructure.contant.role.RoleCustomer;
+import com.example.demo.infrastructure.contant.role.RoleOwner;
 import com.example.demo.repositories.AdminRepository;
 import com.example.demo.repositories.ApprovalHistoryRepository;
 import com.example.demo.repositories.BookingRepository;
@@ -136,6 +139,7 @@ public class DBGenerator implements CommandLineRunner {
         admin1.setUsername("huytq@gmail.com");
         admin1.setPassword("12345678");
         admin1.setAvatarUrl("avcqeqw");
+        admin1.setRoleAdmin(RoleAdmin.ADMIN);
         adminRepository.save(admin1);
 
         Admin admin2 = new Admin();
@@ -148,6 +152,7 @@ public class DBGenerator implements CommandLineRunner {
         admin2.setUsername("cuongnq@gmail.com");
         admin2.setPassword("12345678");
         admin2.setAvatarUrl("xyzjkl");
+        admin2.setRoleAdmin(RoleAdmin.ADMIN);
         adminRepository.save(admin2);
 
         //user
@@ -160,6 +165,7 @@ public class DBGenerator implements CommandLineRunner {
         user1.setEmail("sangvt@gmail.com");
         user1.setUsername("sangvt@gmail.com");
         user1.setPassword("12345678");
+        user1.setRoleCustomer(RoleCustomer.CUSTOMER);
         userRepository.save(user1);
 
         User user2 = new User();
@@ -171,6 +177,7 @@ public class DBGenerator implements CommandLineRunner {
         user2.setEmail("tuanvt@gmail.com");
         user2.setUsername("tuanvt@gmail.com");
         user2.setPassword("12345678");
+        user2.setRoleCustomer(RoleCustomer.CUSTOMER);
         userRepository.save(user2);
 
         //owner homestay
@@ -182,6 +189,7 @@ public class DBGenerator implements CommandLineRunner {
         ownerHomestay1.setUsername("cuongnguyen.nd2015@gmail.com");
         ownerHomestay1.setPassword("12345678");
         ownerHomestay1.setGender(true);
+        ownerHomestay1.setRoleOwner(RoleOwner.OWNER);
         ownerHomestayRepository.save(ownerHomestay1);
 
         OwnerHomestay ownerHomestay2 = new OwnerHomestay();
@@ -192,6 +200,7 @@ public class DBGenerator implements CommandLineRunner {
         ownerHomestay2.setUsername("lynv@gmail.com");
         ownerHomestay2.setPassword("12345678");
         ownerHomestay2.setGender(false);
+        ownerHomestay2.setRoleOwner(RoleOwner.OWNER);
         ownerHomestayRepository.save(ownerHomestay2);
 
         //promotion
