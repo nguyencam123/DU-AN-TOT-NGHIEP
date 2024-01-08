c430c1466f92c21c9228dd69d5e1ff6107379402
sangvuong <sangvtph26073@fpt.edu.vn>
2024-01-08 16:30:31 +0700

    getConvenientType, sua getConvenient

--- BE/src/main/java/com/example/demo/cors/customer/repository/CustomerConvenientHomestayRepository.java
+++ BE/src/main/java/com/example/demo/cors/customer/repository/CustomerConvenientHomestayRepository.java
@@ -1,8 +1,19 @@
 package com.example.demo.cors.customer.repository;
 
+import com.example.demo.cors.customer.model.request.CustomerConvenientHomestayRequest;
+import com.example.demo.entities.ConvenientHomestay;
 import com.example.demo.repositories.ConvenientHomestayRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerConvenientHomestayRepository extends ConvenientHomestayRepository {
+
+    @Query(value = """
+            SELECT * FROM convenient_homestay a
+            WHERE a.convenient_homestay_type_id =:#{#request.convenientHomestayTypeId}
+            """, nativeQuery = true)
+    List<ConvenientHomestay> getAllConvenientHomestayType(CustomerConvenientHomestayRequest request);
 }
