3e77630c9dcfebf91f137ca90fe35c22f8228182
nguyencam123 <camnmph26581@fpt.edu.vn>
2024-01-09 16:48:48 +0700

    config bug log 9/1

--- src/layout/layoutadmin/headerAdmin.js
+++ src/layout/layoutadmin/headerAdmin.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import { Layout, Menu } from 'antd';
-import Title from 'antd/es/typography/Title';
-import logotravel from "../../assets/svg/Rectangle 405.svg"
-import {
-    AppleOutlined
-} from '@ant-design/icons';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { logoutUser } from '../../features/user/userThunk';
-const { Header } = Layout;
+import React from 'react'
+import { Avatar, Layout, Menu } from 'antd'
+import Title from 'antd/es/typography/Title'
+import logotravel from '../../assets/svg/Rectangle 405.svg'
+import avartadamin from '../../assets/img/TravelVivu.jpg'
+import { AppleOutlined } from '@ant-design/icons'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { logoutUser } from '../../features/user/userThunk'
+const { Header } = Layout
 const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-}));
+  key,
+  label: `nav ${key}`,
+}))
 const HeaderAdmin = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate();
-    const logout = () => {
-        dispatch(logoutUser())
-        navigate('/login-admin')
-    }
-    const userDetail = JSON.parse(localStorage.getItem('adminDetail'));
-    const nameUser = userDetail?.data.name;
-    return (
-        <Header style={{ display: 'flex', alignItems: 'center' }}>
-            <div className="demo-logo" />
-            <Title level={3} style={{ marginTop: '13px', color: 'white' }}><img src={logotravel} />Trang quản trị Homestay của bạn</Title>
-            <div className="layout-login" style={{ marginLeft: 'auto' }}><Title level={5} style={{ color: 'white', display: 'flex' }}>{nameUser} <div style={{ cursor: 'pointer' }} onClick={logout}>(Đăng xuất)</div></Title></div>
-        </Header>
-    )
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const logout = () => {
+    dispatch(logoutUser())
+    navigate('/login-admin')
+  }
+  const userDetail = JSON.parse(localStorage.getItem('adminDetail'))
+  const nameUser = userDetail?.data.name
+  return (
+    <Header style={{ display: 'flex', alignItems: 'center' }}>
+      <div className='demo-logo' />
+      <Title level={3} style={{ marginTop: '13px', color: 'white' }}>
+        <img src={logotravel} />
+        Trang quản trị Homestay của bạn
+      </Title>
+      <div className='layout-login' style={{ marginLeft: 'auto' }}>
+        <Title level={5} style={{ color: 'white', display: 'flex' }}>
+          {nameUser}{' '}
+          <div style={{ cursor: 'pointer' }} onClick={logout}>
+            (Đăng xuất)
+          </div>
+        </Title>
+      </div>
+    </Header>
+  )
 }
 export default HeaderAdmin
